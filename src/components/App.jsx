b126b36c03a4c2b84cf2d3ef9a163ded8a9f3814
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-06 16:41:19 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Section } from './Section';
+import { Statistics } from './Statistics';
+
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleLeaveFeedback = type => {
+    setFeedback(prev => ({
+      ...prev,
+      [type]: prev[type] + 1,
+      
+    }));
+  };
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage =
+    total > 0 ? Math.round((feedback.good / total) * 100) : 0;
   return (
     <div
       style={{
@@ -7,10 +29,24 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Section title="Leave your Feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      </Section>
     </div>
   );
 };
